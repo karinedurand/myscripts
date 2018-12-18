0663ecf2590f514a80a285d97ea868e5aec37475
karinedurand <durandkarine5058@gmail.com>
2018-12-18 10:11:54 +0100

    upadte Na

--- SCRIPT_FASTSIMBACOK/fastsimbac_IM.sh
+++ SCRIPT_FASTSIMBACOK/fastsimbac_IM.sh
@@ -9,7 +9,7 @@
 #N1 size pop1
 #N2 size pop2
 #Ts time scale of split
-for i in locus*
+for i in `cat listems`
 do while read line 
 do L=`echo $line |cut -d" " -f1`
 t=`echo $line |cut -d" " -f2`
@@ -18,10 +18,11 @@ delta=`echo $line |cut -d" " -f4`
 TS=`echo $line |cut -d" " -f5`
 N1=`echo $line |cut -d" " -f6`
 N2=`echo $line |cut -d" " -f7`
-M12=`echo $line | cut -d" " -f8`
-M21=`echo $line | cut -d" " -f9`
-echo ./fastSimBac 30 $L -T -t $t -r $r $delta  -I 2 13 17 -ej $TS 1 2  -n 1 $N1 -n 2 $N2   -m  1 2  $M12  -m  2 1  $M21 >line
-   ./fastSimBac 30 $L -T -t $t -r $r $delta  -I 2 13 17 -ej $TS 1 2  -n 1 $N1 -n 2 $N2   -m  1 2  $M12  -m  2 1  $M21  | ./msformatter >temp2 
+Na=`echo $line |cut -d" " -f8`
+M12=`echo $line | cut -d" " -f9`
+M21=`echo $line | cut -d" " -f10`
+echo ./fastSimBac 30 $L  -t $t -r $r $delta  -I 2 13 17  -n 1 $N1 -n 2 $N2   -m  1 2  $M12  -m  2 1  $M21 -ej $TS 1 2 -eN $TS $Na >line
+   ./fastSimBac 30 $L  -t $t -r $r $delta  -I 2 13 17   -n 1 $N1 -n 2 $N2   -m  1 2  $M12  -m  2 1  $M21 -ej $TS 1 2 -eN $TS $Na  | ./msformatter >temp2 
 if test -s temp2
 then 
 echo "temp2" >>log
