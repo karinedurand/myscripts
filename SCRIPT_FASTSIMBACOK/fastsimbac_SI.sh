7d74e316270760edbc37fc9391ac8fbe8bbe46eb
karinedurand <durandkarine5058@gmail.com>
2018-12-20 10:09:10 +0100

    update TS2

--- SCRIPT_FASTSIMBACOK/fastsimbac_SI.sh
+++ SCRIPT_FASTSIMBACOK/fastsimbac_SI.sh
@@ -10,8 +10,9 @@ while read line
   N1=`echo $line |cut -d" " -f6`
   N2=`echo $line |cut -d" " -f7`
   Na=`echo $line |cut -d" " -f8`
-  echo   ./fastSimBac 30 $L -T -t $t -r $r $delta -I 2 13 17  -ej $TS 1 2  -eN $TS $Na > line
-    ./fastSimBac 30 $L -T -t $t -r $r $delta -I 2 13 17  -ej $TS 1 2  -eN $TS $Na | ./msformatter >temp2 
+  TS2=`echo $line |cut -d" " -f9`
+  echo   ./fastSimBac 30 $L -T -t $t -r $r $delta -I 2 13 17  -ej $TS 1 2  -eN $TS2 $Na > line
+    ./fastSimBac 30 $L -T -t $t -r $r $delta -I 2 13 17  -ej $TS 1 2  -eN $TS2 $Na | ./msformatter >temp2 
 if test -s temp2
 then 
 echo "temp2" >>log
