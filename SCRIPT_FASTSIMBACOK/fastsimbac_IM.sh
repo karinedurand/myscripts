e0c1129637c937376dae54acabdde53f9116e901
karinedurand <durandkarine5058@gmail.com>
2018-12-20 11:15:20 +0100

    load module

--- SCRIPT_FASTSIMBACOK/fastsimbac_IM.sh
+++ SCRIPT_FASTSIMBACOK/fastsimbac_IM.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+module load bioinfo/fastsimbac-bd3ad13
 #export LD_LIBRARY_PATH=/root/Priors_IM_10000/myParadigmLib/usr/lib/x86_64-linux-gnu
 #IM
 ####Param_demo (5) =   N1, N2, Ts, M12, M21,# -I 2 13 17 -m 1 2   M12 -m 2 1   M21  -en Te1 1  N’1 -en  Te2 2 N’2  -n  1  N1 -n  2 N2 -ej Ts 1 2
@@ -22,8 +23,8 @@ Na=`echo $line |cut -d" " -f8`
 M12=`echo $line | cut -d" " -f9`
 M21=`echo $line | cut -d" " -f10`
 TS2=`echo $line | cut -d" " -f11`
-echo ./fastSimBac 30 $L  -t $t -r $r $delta  -I 2 13 17  -n 1 $N1 -n 2 $N2   -m  1 2  $M12  -m  2 1  $M21 -ej $TS 1 2 -eN $TS $Na >line
-   ./fastSimBac 30 $L  -t $t -r $r $delta  -I 2 13 17   -n 1 $N1 -n 2 $N2   -m  1 2  $M12  -m  2 1  $M21 -ej $TS 1 2 -eN $TS $Na  | ./msformatter >temp2 
+echo ./fastSimBac 30 $L  -t $t -r $r $delta  -I 2 13 17  -n 1 $N1 -n 2 $N2   -m  1 2  $M12  -m  2 1  $M21 -ej $TS 1 2 -eN $TS2 $Na >line
+   ./fastSimBac 30 $L  -t $t -r $r $delta  -I 2 13 17   -n 1 $N1 -n 2 $N2   -m  1 2  $M12  -m  2 1  $M21 -ej $TS 1 2 -eN $TS2 $Na  | ./msformatter >temp2 
 if test -s temp2
 then 
 echo "temp2" >>log
