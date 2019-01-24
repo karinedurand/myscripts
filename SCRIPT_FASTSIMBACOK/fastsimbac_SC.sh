59301aeb8ad23e17be82acfe9979d67c52c3ed0a
karinedurand <durandkarine5058@gmail.com>
2019-01-24 09:20:34 +0100

    upadte align

--- SCRIPT_FASTSIMBACOK/fastsimbac_SC.sh
+++ SCRIPT_FASTSIMBACOK/fastsimbac_SC.sh
@@ -11,20 +11,20 @@ module load bioinfo/fastsimbac-bd3ad13
 #Ts time scale of split SC
 for i in `cat listems`
 do while read line 
-do L=`echo $line |cut -d" " -f1`
-t=`echo $line |cut -d" " -f2`
-r=`echo $line |cut -d" " -f3`
-delta=`echo $line |cut -d" " -f4`
-TS=`echo $line |cut -d" " -f5`
-N1=`echo $line |cut -d" " -f6`
-N2=`echo $line |cut -d" " -f7`
-Na=`echo $line |cut -d" " -f8`
-M12=`echo $line | cut -d" " -f9`
-M21=`echo $line | cut -d" " -f10`
-Tsc=`echo $line | cut -d" " -f11`
-TS2=`echo $line | cut -d" " -f12`
-echo ./fastSimBac 30 $L -T -t $t -r $r $delta -I 2 13 17   -n 1 $N1 -n 2 $N2   -m  1 2  $M12  -m  2 1  $M21 -eM $Tsc  0  -ej $TS 1 2 -eN $TS2 $Na  > line
-fastSimBac 30 $L  -t $t -r $r $delta -I 2 13 17   -n 1 $N1 -n 2 $N2   -m  1 2  $M12  -m  2 1  $M21 -eM $Tsc  0  -ej $TS 1 2  -eN $TS2 $Na | msformatter >temp2 
+do #L=`echo $line |cut -d" " -f1`
+t=`echo $line |cut -d" " -f1`
+r=`echo $line |cut -d" " -f2`
+delta=`echo $line |cut -d" " -f3`
+TS=`echo $line |cut -d" " -f4`
+#N1=`echo $line |cut -d" " -f6`
+N2=`echo $line |cut -d" " -f5`
+Na=`echo $line |cut -d" " -f6`
+M12=`echo $line | cut -d" " -f7`
+M21=`echo $line | cut -d" " -f8`
+Tsc=`echo $line | cut -d" " -f9`
+TS2=`echo $line | cut -d" " -f10`
+echo ./fastSimBac 30 $L -T -t $t -r $r $delta -I 2 13 17   -n 1 1 -n 2 $N2   -m  1 2  $M12  -m  2 1  $M21 -eM $Tsc  0  -ej $TS 1 2 -eN $TS2 $Na  > line
+fastSimBac 30 $L  -t $t -r $r $delta -I 2 13 17   -n 1 1 -n 2 $N2   -m  1 2  $M12  -m  2 1  $M21 -eM $Tsc  0  -ej $TS 1 2  -eN $TS2 $Na | msformatter >temp2 
 if test -s temp2
 then 
 echo "temp2" >>log
