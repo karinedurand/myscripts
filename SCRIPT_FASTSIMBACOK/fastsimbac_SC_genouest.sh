00663025a49e3f376b07d88352720bb0ec23ee1e
karinedurand <durandkarine5058@gmail.com>
2019-01-28 09:48:11 +0100

    UIPDATE

--- SCRIPT_FASTSIMBACOK/fastsimbac_SC_genouest.sh
+++ SCRIPT_FASTSIMBACOK/fastsimbac_SC_genouest.sh
@@ -14,7 +14,7 @@ cd "FILE"$SLURM_ARRAY_TASK_ID
    N2=`cat "PriorsSC-"$SLURM_ARRAY_TASK_ID |cut -d" " -f5`
    M12=`cat "PriorsSC-"$SLURM_ARRAY_TASK_ID | cut -d" " -f7`
    M21=`cat "PriorsSC-"$SLURM_ARRAY_TASK_ID | cut -d" " -f8`
-   Tsc=`echo $line | cut -d" " -f9`
+   Tsc=`cat "PriorsSC-"$SLURM_ARRAY_TASK_ID  | cut -d" " -f9`
    echo   ./fastSimBac 30 1712736  -T -t $t -r $r $delta -I 2 13 17   -n 1 1 -n 2 $N2   -m  1 2  $M12  -m  2 1  $M21 -eM $Tsc  0  -ej $TS 1 2    > $SLURM_ARRAY_TASK_ID".line"
    fastSimBac 30 1712736 -T -t $t -r $r $delta -I 2 13 17   -n 1 1 -n 2 $N2   -m  1 2  $M12  -m  2 1  $M21 -eM $Tsc  0  -ej $TS 1 2    | msformatter > $SLURM_ARRAY_TASK_ID".temp2" 
    if test -s $SLURM_ARRAY_TASK_ID.temp2
